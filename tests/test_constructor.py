8a7725330711627996958e16a6817dad5b29df98
Nepo <braulio.rojas@islas.org.mx>
2020-07-06 16:18:24 -0700

    Agrega prueba para la importación de los recursos tabular data

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,6 +1,10 @@
 from Tablanal import DataTable
 import pytest
+import metadatatools as mdt
 
+def test_import_tabludar_data_resource():
+    file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
+    DatosSocorro = mdt.import_tabular_data_resource(file_dtp_path)
 
 def test_ConstructorVacio():
     Tabla = DataTable()
