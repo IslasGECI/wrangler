036b2e7ff17887cad0db7ef5f90b9bd018e7eb79
Nepo <braulio.rojas@islas.org.mx>
2020-07-15 12:02:12 -0700

    Da formato utilzando black

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,11 +2,12 @@ from Tablanal import DataTable
 import pytest
 import metadatatools as mdt
 
+
 def test_import_tabludar_data_resource():
     file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
     DatosSocorro = mdt.import_tabular_data_resource(file_dtp_path)
 
+
 def test_ConstructorVacio():
     Tabla = DataTable()
     assert type(Tabla).__name__ == "DataTable"
-
