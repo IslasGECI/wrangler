d3bc03c466b0f7e0ad4388257f2c63102f535bed
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-08-11 11:02:01 -0700

    Cambia la dirección del archivo

---
+++ tests/test_getNamesFromAxis.py
@@ -0,0 +1,12 @@
+from Tablanal import Tablanal
+
+
+import toolboxes
+
+archivoPrueba = toolboxes.findInDataPath("ARCHIVO_PARA_EVALUAR_PRUEBAS.xlsx")
+
+def test_getNameFromAxis():
+    """Prueba que la función extrae el nombre de la columna con axis T"""
+    Tabla = Tablanal.readExcel(archivoPrueba)
+    nombreEjeTiempo = Tabla.getNameFromAxis(IndependentVariable.T)
+    assert nombreEjeTiempo == "Fecha"
