42c8009b366460d357459f081c52547ddf982166
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-02-18 14:02:41 -0800

    ✅ Test add property

--- tests/test_datapackages.py
+++ tests/test_datapackages.py
@@ -7,3 +7,13 @@ def test_Field():
     name_expected = diccionario["name"]
     name_obtained = Campo.name
     assert name_expected == name_obtained
+
+def test_Field_add_property():
+    diccionario = {"name": "Nombre"}
+    Campo = mdt.Field(diccionario)
+    expected_value = 0
+    name_property = "new_property"
+    Campo.add_property(name_property, expected_value)
+    dict_field = Campo.get_dict()
+    obtained_value = dict_field[name_property]
+    assert expected_value == obtained_value
