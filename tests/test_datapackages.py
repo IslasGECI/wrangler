c79ff9cc5beb15d5b201b3db684709770daca3c6
Xavier Paredes <xavier.paredes@islas.org.mx>
2021-03-19 13:19:48 -0700

    😒✅ change variable name

--- tests/test_datapackages.py
+++ tests/test_datapackages.py
@@ -2,28 +2,28 @@ import metadatatools as mdt
 
 
 diccionario = {"name": "Nombre"}
-Campo = mdt.Field(diccionario)
+Field = mdt.Field(diccionario)
 
 
 def test_Field():
     name_expected = diccionario["name"]
-    name_obtained = Campo.name
+    name_obtained = Field.name
     assert name_expected == name_obtained
 
 
 def test_Field_add_property():
     expected_value = 0
     name_property = "new_property"
-    Campo.add_property(name_property, expected_value)
-    dict_field = Campo.get_dict()
-    obtained_value = dict_field[name_property]
+    Field.add_property(name_property, expected_value)
+    dictionary_field = Field.get_dict()
+    obtained_value = dictionary_field[name_property]
     assert expected_value == obtained_value
 
 
 def test_Field_modify_property():
     expected_value = 2
     name_property = "new_property"
-    Campo.modify_property(name_property, expected_value)
-    dict_field = Campo.get_dict()
-    obtained_value = dict_field[name_property]
+    Field.modify_property(name_property, expected_value)
+    dictionary_field = Field.get_dict()
+    obtained_value = dictionary_field[name_property]
     assert expected_value == obtained_value
