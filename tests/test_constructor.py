1eda9e331c2109868143b15156327cc02545b9b7
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-20 11:22:37 -0700

    🎨 Make format with black

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,4 +4,3 @@ import metadatatools as mdt
 def test_import_tabludar_data_resource():
     file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
     DatosSocorro = mdt.import_tabular_data_resource(file_dtp_path)
-
