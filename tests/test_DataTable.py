fca6ff8f50074b49dd0c65034568fddd8af0c57d
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-19 21:11:54 -0700

    ✅ Tests TypeError

--- tests/test_DataTable.py
+++ tests/test_DataTable.py
@@ -1,5 +1,6 @@
 import numpy as np
 import metadatatools as mdt
+import pytest
 
 file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
 SocorroData = mdt.import_tabular_data_resource(file_dtp_path)
@@ -9,13 +10,21 @@ def test_get_variable_name_from_standard_name():
     expected_name: str = "Esfuerzo"
     obtained_name: str = SocorroData.get_variable_name_from_standard_name(mdt.StandardName.effort)
     assert expected_name == obtained_name
+    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+        obtained_name: str = SocorroData.get_variable_name_from_standard_name("Hola")
 
 
 def test_get_variable_name_from_axis():
     expected_name: str = "Fecha"
     obtained_name: str = SocorroData.get_variable_name_from_axis(mdt.Axis.T)
     assert expected_name == obtained_name
+    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+        obtained_name: str = SocorroData.get_variable_name_from_axis("Hola")
 
+
+def test_add_variable_metadata():
+    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+        obtained_name: str = SocorroData.add_variable_metadata("Hola")
 
 def test_get_value():
     column_name: str = "Capturas"
