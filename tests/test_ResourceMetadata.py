059b8211f2ecfbe7dd5142beb908398e65f4d05f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-20 13:46:53 -0700

    ✅ Tests builders

---
+++ tests/test_ResourceMetadata.py
@@ -0,0 +1,13 @@
+import metadatatools as mdt
+
+ResourceMetadata = mdt.Metadata._ResourceMetadata.ResourceMetadata()
+
+def test_init_():
+    assert ResourceMetadata.id == None
+    assert ResourceMetadata.description == None
+    assert ResourceMetadata.name == None
+    assert ResourceMetadata.path == None
+    assert ResourceMetadata.profile == None
+    assert ResourceMetadata.source == None
+    assert ResourceMetadata.title == None
+    assert ResourceMetadata.titulo == None
---
+++ tests/test_VariableMetadata.py
@@ -0,0 +1,13 @@
+import metadatatools as mdt
+
+VariableMetadata = mdt.Metadata._VariableMetadata.VariableMetadata()
+
+def test_init_():
+    assert VariableMetadata.axis == None
+    assert VariableMetadata.description == None
+    assert VariableMetadata.name == None
+    assert VariableMetadata.long_name == None
+    assert VariableMetadata.nombre_largo == None
+    assert VariableMetadata.standard_name == None
+    assert VariableMetadata.units == None
+    assert VariableMetadata.type == None
