1f0ece02eba43224c22caa235e8392aaea43bef9
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-30 18:16:41 -0700

    ✅🐛 Add anchor regex

--- tests/test_DataTable.py
+++ tests/test_DataTable.py
@@ -13,7 +13,7 @@ def test_get_variable_name_from_standard_name():
     expected_name: str = "Esfuerzo"
     obtained_name: str = SocorroData.get_variable_name_from_standard_name(mdt.StandardName.effort)
     assert expected_name == obtained_name
-    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+    with pytest.raises(TypeError, match=r"^Se espera un objeto de la clase"):
         obtained_name: str = SocorroData.get_variable_name_from_standard_name("Hola")
 
 
@@ -21,12 +21,12 @@ def test_get_variable_name_from_axis():
     expected_name: str = "Fecha"
     obtained_name: str = SocorroData.get_variable_name_from_axis(mdt.Axis.T)
     assert expected_name == obtained_name
-    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+    with pytest.raises(TypeError, match=r"^Se espera un objeto"):
         obtained_name: str = SocorroData.get_variable_name_from_axis("Hola")
 
 
 def test_add_variable_metadata():
-    with pytest.raises(TypeError, match=r"Se espera un objeto de la clase*"):
+    with pytest.raises(TypeError, match=r"^Se espera un objeto de la clase"):
         obtained_name: str = SocorroData.add_variable_metadata("Hola")
 
 
