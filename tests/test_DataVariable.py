b0e539d6a08ff7e38c77ec805e91d36bdb61da06
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-03-30 19:26:19 -0700

    🏹👾✅ Hunt mutant in IDataVariable

--- tests/test_DataVariable.py
+++ tests/test_DataVariable.py
@@ -1,4 +1,5 @@
 import metadatatools as mdt
+import pytest
 
 Variable = mdt.Metadata.DataVariable()
 
@@ -8,3 +9,8 @@ def test_init():
     variable_metadata = Variable.get_variable_metadata()
     class_variable_metadata = mdt.Metadata._VariableMetadata.VariableMetadata
     assert isinstance(variable_metadata, class_variable_metadata)
+
+
+def test_IDataVariable():
+    with pytest.raises(TypeError, match=r"abstract methods get_variable_metadata$"):
+        DataVariable = mdt.Metadata.IDataVariable()
