4f9cabe120adbadda951b530424d5ebcbca6d39c
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-06 17:20:46 -0700

    🏹👾✅ Hunt mutant in datapackage

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,6 +1,15 @@
 import metadatatools as mdt
 
 
-def test_import_tabludar_data_resource():
-    file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
-    DatosSocorro = mdt.import_tabular_data_resource(file_dtp_path)
+file_dtp_path: str = "tests/data/erradicaciones-mamiferos/captura_gatos_socorro.csv"
+SocorroData = mdt.import_tabular_data_resource(file_dtp_path)
+
+def test_datapackages():
+    assert SocorroData.metadatos.profile == 'tabular-data-resource'
+    assert SocorroData.metadatos_variables[2].long_name == ''
+    assert SocorroData.metadatos_variables[2].description == ''
+    assert SocorroData.metadatos_variables[2].nombre_largo == ''
+    assert SocorroData.metadatos_variables[2].standard_name == ''
+    assert SocorroData.metadatos_variables[1].axis == ''
+    assert SocorroData.metadatos_variables[2].units == ''
+    assert SocorroData.metadatos_variables[2].type == ''
