3b7131dce39bf95fe2433cf280da990a2517117f
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-07 19:45:09 -0700

    🏹👾✅ Hunt mutant in interface

--- tests/test_ResourceMetadata.py
+++ tests/test_ResourceMetadata.py
@@ -1,4 +1,5 @@
 import metadatatools as mdt
+import pytest
 
 ResourceMetadata = mdt.Metadata._ResourceMetadata.ResourceMetadata()
 
@@ -19,3 +20,20 @@ def test_properties():
     assert ResourceMetadata.profile == "Nombre"
     ResourceMetadata.name = "Nombre-12.56"
     assert ResourceMetadata.id == "Nombre1256"
+
+
+def test_IResourceMetadata():
+    with pytest.raises(
+        TypeError,
+        match=r"abstract methods description, id, name, path, profile, source, title, titulo$",
+    ):
+        mdt.Metadata.IResourceMetadata()
+
+    assert isinstance(mdt.Metadata.IResourceMetadata.description, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.id, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.name, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.path, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.profile, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.source, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.title, property)
+    assert isinstance(mdt.Metadata.IResourceMetadata.titulo, property)
--- tests/test_VariableMetadata.py
+++ tests/test_VariableMetadata.py
@@ -1,4 +1,5 @@
 import metadatatools as mdt
+import pytest
 
 VariableMetadata = mdt.Metadata._VariableMetadata.VariableMetadata()
 
@@ -12,3 +13,18 @@ def test_init_():
     assert VariableMetadata.standard_name is None
     assert VariableMetadata.units is None
     assert VariableMetadata.type is None
+
+
+def test_IVariableMetadata():
+    with pytest.raises(
+        TypeError,
+        match=r"abstract methods axis, description, long_name, name, nombre_largo, standard_name, units$",
+    ):
+        mdt.Metadata.IVariableMetadata()
+
+    assert isinstance(mdt.Metadata.IVariableMetadata.axis, property)
+    assert isinstance(mdt.Metadata.IVariableMetadata.name, property)
+    assert isinstance(mdt.Metadata.IVariableMetadata.long_name, property)
+    assert isinstance(mdt.Metadata.IVariableMetadata.nombre_largo, property)
+    assert isinstance(mdt.Metadata.IVariableMetadata.standard_name, property)
+    assert isinstance(mdt.Metadata.IVariableMetadata.units, property)
