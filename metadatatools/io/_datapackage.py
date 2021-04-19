7af6f967370ac1e4acf92e50c756032c427494cc
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-19 15:19:56 -0700

    👩🏿‍💻 Change language

--- metadatatools/io/_datapackage.py
+++ metadatatools/io/_datapackage.py
@@ -8,44 +8,44 @@ from ..Metadata import DataTable, ResourceMetadata, VariableMetadata
 
 def import_tabular_data_resource(path: str):
     """
-    Esta función te permite importar el recurso de un datapackage.
+    Esta función te permite importar el resource de un datapackage.
 
     # Parámetros
 
     `path str`
-    Dirección al recurso que quieres importar
+    Dirección al resource que quieres importar
     """
-    direccion_datapackage, direccion_recurso = os.path.split(path)
+    head_path, tail_path = os.path.split(path)
     with open(
-        f"{direccion_datapackage}/datapackage.json", "r", encoding="utf-8"
-    ) as archivo_metadatos:
-        diccionario_metadatos = json.load(archivo_metadatos)
+        f"{head_path}/datapackage.json", "r", encoding="utf-8"
+    ) as metadata_file:
+        metadata_dictionary = json.load(metadata_file)
     tabla_datos = pd.read_csv(path)
     data_table = DataTable()
-    for recurso in diccionario_metadatos["resources"]:
-        if recurso["path"] == direccion_recurso:
-            diccionario_metadatos_variables = recurso
-    metadatos_recurso = _build_metadata(diccionario_metadatos_variables, direccion_datapackage)
-    data_table.metadata = metadatos_recurso
-    add_variable_metadata(data_table, diccionario_metadatos_variables)
+    for resource in metadata_dictionary["resources"]:
+        if resource["path"] == tail_path:
+            varible_metadata_dictionary = resource
+    resource_metadata = _build_metadata(varible_metadata_dictionary, head_path)
+    data_table.metadata = resource_metadata
+    add_variable_metadata(data_table, varible_metadata_dictionary)
     data_table.data = tabla_datos
     return data_table
 
 
-def _build_metadata(metadata, direccion_datapackage):
-    metadatos_recurso = ResourceMetadata()
-    metadatos_recurso.name = metadata.get("name", "")
-    metadatos_recurso.description = metadata.get("description", "")
-    metadatos_recurso.path = direccion_datapackage
-    metadatos_recurso.profile = metadata.get("profile", "")
-    metadatos_recurso.source = metadata.get("sources", "")
-    metadatos_recurso.title = metadata.get("title", "")
-    metadatos_recurso.titulo = metadata.get("titulo", "")
-    return metadatos_recurso
+def _build_metadata(metadata, head_path):
+    resource_metadata = ResourceMetadata()
+    resource_metadata.name = metadata.get("name", "")
+    resource_metadata.description = metadata.get("description", "")
+    resource_metadata.path = head_path
+    resource_metadata.profile = metadata.get("profile", "")
+    resource_metadata.source = metadata.get("sources", "")
+    resource_metadata.title = metadata.get("title", "")
+    resource_metadata.titulo = metadata.get("titulo", "")
+    return resource_metadata
 
 
-def add_variable_metadata(data_table, diccionario_metadatos_variables):
-    for diccionario_metadatos_variable in diccionario_metadatos_variables["schema"]["fields"]:
+def add_variable_metadata(data_table, varible_metadata_dictionary):
+    for diccionario_metadatos_variable in varible_metadata_dictionary["schema"]["fields"]:
         metadatos_variable = VariableMetadata()
         metadatos_variable.name = diccionario_metadatos_variable.get("name", "")
         metadatos_variable.long_name = diccionario_metadatos_variable.get("long_name", "")
